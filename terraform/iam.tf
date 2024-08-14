f31450b3477dc541ae636bcb2c47756a44c0f783
Dieg0Code <diegoobando20@gmail.com>
2024-08-14 03:31:02 -0400

    update iam policy for api_products

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -52,3 +52,23 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+resource "aws_iam_policy" "lambda_invoke_policy" {
+  name        = "lambda_invoke_policy"
+  description = "Policy to allow Lambda functions to invoke other Lambda functions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "lambda:InvokeFunction"
+        Effect   = "Allow"
+        Resource = "arn:aws:lambda:sa-east-1:992382698192:function:scraper"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_invoke_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
+}
