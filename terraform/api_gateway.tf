9992da82cdb9872d987022c43f58fa9332e8e9c9
Dieg0Code <diegoobando20@gmail.com>
2024-08-15 07:02:44 -0400

    zzz

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -198,5 +198,5 @@ resource "aws_api_gateway_stage" "api_stage" {
 }
 
 output "api_gateway_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.sa-east-1.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}/api/v1/products"
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.sa-east-1.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}/api/v1/"
 }
